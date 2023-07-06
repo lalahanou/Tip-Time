98b4556ce119acb6db36e6a7d006f48fafdf3057
Hana <bahloul.hana@gmail.com>
2023-07-06 17:26:15 +0100

    Modifier les ressources de chaines necessaires

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -93,7 +93,7 @@ fun EditNumberField(
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(stringResource(R.string.cost_of_service)) },
+        label = { Text(stringResource(R.string.bill_amount)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
